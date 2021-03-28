e37200c8d662bcb638deb1de3d38fa49c09bd2f7
Dang <haidang.vo@ogilvy.com>
2021-03-28 22:15:24 +0700

    remove used code

--- src/assets/js/libs/RandomString.js
+++ src/assets/js/libs/RandomString.js
@@ -1,10 +1,14 @@
 const alphabet = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm'
 
+const pool = {}
+
 export default function random(length = 16) {
   let str = ''
   while (length) {
     length--
     str += alphabet[~~(Math.random() * alphabet.length)]
   }
+  if (pool[str]) return random()
+  pool[str] = str
   return str
 }
